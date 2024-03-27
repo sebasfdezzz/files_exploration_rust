9441025a332d1c2bbbdf86bff9624ec80ff4b1ce
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2024-03-27 10:26:06 -0600

    counter example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-fn main() {
-    MainWindow::new().unwrap().run().unwrap();
-}
+// fn main() {
+//     MainWindow::new().unwrap().run().unwrap();
+// }
 
-slint::slint! {
-    export component MainWindow inherits Window {
-        Text {
-            text: "hello world";
-            color: green;
+// slint::slint! {
+//     export component MainWindow inherits Window {
+//         Text {
+//             text: "hello world";
+//             color: green;
+//         }
+//     }
+// }
+
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
         }
-    }
+    });
+
+    ui.run()
 }
