3aacbfa5be0dc7bf9f347081fb65c55e48ccf9cc
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2024-03-27 10:40:20 -0600

    jugando

--- src/main.rs
+++ src/main.rs
@@ -24,5 +24,13 @@ fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
+    ui.on_request_increase_value2({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 2);
+        }
+    });
+
     ui.run()
 }
